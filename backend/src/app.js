34257b2f060dbf85b4e03d5040cc67ac13c44a69
GabrielSilva0109 <silvagabriel0109@gmail.com>
2024-02-08 18:06:41 -0300

    Backend 1.2
    Pasta Routes criada junto com as primeiras rotas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,17 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const Routes = require('./routes/route.js')
 
 const app = express()
 const port = 3001
 
-app.use(bodyParser.json())
+app.use(express.json())
 
 //Rota
 app.get('/', (req, res) => {
     res.send("Servidor On!")
 })
+
+app.use('/', Routes)
 
 app.listen(port , () =>{
     console.log(`Servidor rodando na porta ${port}`)
---
+++ backend/src/routes/route.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/')
+router.get('/:id')
+router.post('/')
+router.put('/:id')
+router.delete('/:id')
+
+module.exports = router
