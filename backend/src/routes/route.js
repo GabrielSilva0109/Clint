ecbb1a908a6a8e5449725b926daade5ba1f9e8b4
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-02-19 11:11:24 -0300

    Update route.js

--- backend/src/routes/route.js
+++ backend/src/routes/route.js
@@ -2,6 +2,8 @@ import express  from 'express'
 import { getTasks, getTaskById, createTask, updateTask, deleteTask} from '../controllers/taskController.js'
 
 const router = express.Router()
+
+//Rotas das Tarefas
 router.get('/tasks', getTasks)
 router.get('/task/:id', getTaskById)
 router.post('/task', createTask)
